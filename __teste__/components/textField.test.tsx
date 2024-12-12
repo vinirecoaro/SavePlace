e08220d3dc4d8aade4f7ab8cc4a9a94f0f31848b
Vinicius Recoaro <vrecoaro@hotmail.com>
2024-12-12 13:07:34 -0300

    ComponentTest - TextField - vefiry behavior when feddbak is falsy or truly

---
+++ __teste__/components/textField.test.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import TextField from '@/components/TextField';
+
+describe('TextField Component', () => {
+  it('displays feedback message when feedback is provided', () => {
+    const feedbackMessage = 'Preencha este campo.';
+    const { getByText } = render(
+      <TextField
+        testID="login-input-username"
+        placeholder="Usuário"
+        value=""
+        onChangeText={() => {}}
+        feedback={feedbackMessage}
+        editable={true}
+      />
+    );
+
+    expect(getByText(feedbackMessage)).toBeTruthy();
+  });
+
+  it('does not display feedback message when feedback is not provided', () => {
+    const { queryByText } = render(
+      <TextField
+        testID="login-input-username"
+        placeholder="Usuário"
+        value=""
+        onChangeText={() => {}}
+        feedback=""
+        editable={true}
+      />
+    );
+
+    expect(queryByText(/Preencha este campo./)).toBeNull();
+  });
+});
